100b3e3d962d20f13da9f1b7988700c85baaef8e
Kadir-Ahmet <tekkadirahmet@gmail.com>
2024-05-06 15:22:20 +0300

    güncel hali

---
+++ YemekKitab2/app/src/main/java/com/example/yemekkitab/MainActivity.kt
@@ -0,0 +1,44 @@
+package com.example.yemekkitab
+
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+
+
+        }
+
+    }
+
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+
+        val menuInflater=menuInflater
+        menuInflater.inflate(R.menu.yemek_ekle,menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if(item.itemId==R.id.yemek_ekleme_item){
+
+
+        }
+
+        return super.onOptionsItemSelected(item)
+    }
+
+
+}
